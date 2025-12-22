3fee12c126345a4dda1596554a40c1475cd6baa8
AnatolyHryntsevich <anatolhrynt98@gmail.com>
2025-12-22 15:30:21 +0300

    fix Updated translation script

--- translations_update.sh
+++ translations_update.sh
@@ -1,1 +1,10 @@
-lupdate -recursive . -ts translations/app_ru.ts && lrelease translations/app_en.ts
+echo "Updating translation files..."
+
+# Обновление .ts файлов
+echo "Updating .ts files..."
+lupdate . -ts translations/app_en.ts translations/app_ru.ts
+
+# Компиляция .ts в .qm
+echo "Compiling .qm files..."
+lrelease translations/app_en.ts -qm translations/app_en.qm
+lrelease translations/app_ru.ts -qm translations/app_ru.qm
